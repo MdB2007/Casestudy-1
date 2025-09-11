806dcd3038229a707e66f09e005a4c9de851e3de
MdB562196 <562196@student.fontys.nl>
2025-09-11 15:04:36 +0200

    first commit

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1" # pas aan naar jouw regio
+  profile = "default" # of de naam van jouw profiel in ~/.aws/config)
+}
+
+# Test resource: haalt info op over de caller
+data "aws_caller_identity" "current" {}
+
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+output "caller_arn" {
+  value = data.aws_caller_identity.current.arn
+}
